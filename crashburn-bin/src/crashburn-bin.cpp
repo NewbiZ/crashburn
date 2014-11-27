bacd0f0d5b05440d1306435f14d49a193d5fb16a
newbiz <vallee.aurelien@gmail.com>
2014-11-27 19:17:27 +0200

    Add missing license to main binary

--- crashburn-bin/src/crashburn-bin.cpp
+++ crashburn-bin/src/crashburn-bin.cpp
@@ -1,3 +1,23 @@
+/*
+Copyright (C) 2014 Aurelien Vallee
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
 #include <iostream>
 #include <cstdlib>
 
@@ -37,7 +57,6 @@ public:
         text2_.set_text("This 1st test contains 2 numbers, and a smiley ;-)");
         text2_.set_position(50., 100.);
         text2_.set_color(0., 1., 0.);
-        
 
         return true;
     }
@@ -62,7 +81,7 @@ public:
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        text1_.render();        
+        text1_.render();
         text2_.render();
     }
 
@@ -76,7 +95,7 @@ int main(int /*argc*/, char** /*argv*/)
     crashburn::Engine& engine = crashburn::Engine::instance();
 
     engine.set_scene(new MyTestScene(engine));
-	
+
     engine.start();
 
     return EXIT_SUCCESS;
